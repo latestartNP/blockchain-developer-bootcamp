62cc6d1be01cd9da3d5aa7e879c501a97c9bfdb3
latestartNP <aclews@Andrew_Laptop.localdomain>
2022-08-07 11:31:31 -0400

    mumbai deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,10 @@ module.exports = {
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(','),
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
     }
   },
 };
